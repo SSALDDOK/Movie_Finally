eb7190b0a33700c97ee924ae956348f3c7333936
SSALDDOK <ezz1502@naver.com>
2020-12-28 18:23:46 +0900

    finally movie react

---
+++ src/routes/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./About.css";
+
+function About(){
+    return (
+    <div className="about__container">
+    <span>
+      “React를 통해 axios의 fetch방식과 git호스팅 prop-types등 패키지 설치법과,
+       JSX,Props,state,route의 기능을 공부했습니다.”
+    </span>
+  </div>
+    );
+}
+
+export default About;
